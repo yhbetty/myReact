859cb96ae75c28921ee8d066c7825579fe3dfd7f
yhbetty <yhbettychen@gmail.com>
2024-11-20 16:18:36 +0800

    1120-todo-b

--- myreact-todolist/vite.config.js
+++ myreact-todolist/vite.config.js
@@ -3,5 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  // 建立一個開發中的路徑
+  // process.env.NODE_ENV =>　取得node環境中的變數
+  // 如果目前路徑等於產品路徑，就執行倉庫路徑
+  base:process.env.NODE_ENV === 'production' ?'/myReact/' :'/',
   plugins: [react()],
 })
