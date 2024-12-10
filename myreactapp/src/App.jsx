29e6e4ab0045749c3f96af1b7828fbba08f03971
yhbetty <yhbettychen@gmail.com>
2024-12-10 16:18:16 +0800

    1210-jQuery2

--- myreactapp/src/App.jsx
+++ myreactapp/src/App.jsx
@@ -1,12 +1,32 @@
+import { useEffect } from 'react';
 import $ from 'jquery'
 
 export default function App() {
+
+    // 寫法一：過時寫法
+    // useEffect(() => {
+    //     $('cssAnim1').hover(function () {
+    //         $(this).addClass('imgScale');
+    //     }, function () {
+    //         $(this).removeClass('imgScale');
+    //     })
+    // }, [])
+
+    // 寫法二：建議寫法
+    useEffect(() => {
+        $('.cssAnim1').on('mouseover', function () {
+            $(this).addClass('imgScale');
+        })
+        $('.cssAnim1').on('mouseout', function () {
+            $(this).removeClass('imgScale');
+        })
+    }, [])
+
     return (
         <div>
-            <h2>jquery</h2>
-            <button onClick={() => {
-                console.log($('h2').text())
-            }}>click</button>
+            <a href="#" className="cssAnim1">
+                <img src="./images/3.jpg" alt="" />
+            </a>
         </div>
     )
 }
