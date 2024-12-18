cdf0a2eea96f7ac202636d671c139c470615f6e1
yhbetty <yhbettychen@gmail.com>
2024-12-18 13:00:29 +0800

    1217

--- myreactapp/public/html/jQuery/js/script2.js
+++ myreactapp/public/html/jQuery/js/script2.js
@@ -8,18 +8,13 @@ $(function () {
 
         var setMdw = $(this),
             setHref = setMdw.attr('href'),  // 取出按鈕href屬性值
-            setSourse = $(setHref).html(),    // 取出herf對應id的內容
             wdHeight = $(window).height();   // 取得目前視窗高度
 
-
-        // <!-- <div id="mdOverlay"></div>
-        // <div id="mdWindow">
-        //     <div class="mdClose">X</div>
-        //     <div id="contWrap"></div>
-        // </div> -->
-
         // 插入到網頁尾端的HTML，不包含#modalInclude的元素
-        $('body').append('<div id="mdOverlay"></div><div id="mdWindow"><div class="mdClose">X</div><div id="contWrap">' + setSourse + '</div></div>');
+        $('body').append('<div id="mdOverlay"></div><div id="mdWindow"><div class="mdClose">X</div><div id="contWrap"></div></div>');
+
+        // 取得外部檔案內容
+        $('#contWrap').load(setHref);
 
         // 將產生的彈出視窗與遮罩元素，透明度為0
         $('#mdOverlay, #mdWindow').css({ display: 'block', opacity: '0' });
